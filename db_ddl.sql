4c4ae70bdcfc6ff7a353c4ef372e5ea06fc68007
Igor.Kirillov <Igor.Kirillov@x5.ru>
2021-04-23 19:57:47 +0300

    swagger

---
+++ db_ddl.sql
@@ -0,0 +1,6 @@
+CREATE TABLE mpk (
+     uid varchar(36) NOT NULL,
+     field varchar(256) NULL,
+     CONSTRAINT mpk_pkey PRIMARY KEY (uid)
+);
+
--- src/main/java/ru/x5/mpk/server/Application.java
+++ src/main/java/ru/x5/mpk/server/Application.java
@@ -3,6 +3,7 @@ package ru.x5.mpk.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @SpringBootConfiguration
